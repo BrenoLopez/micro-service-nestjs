e601b38e6fc01b87ce56f972ced40867dcf34345
Breno Lopes de Souza <breno_lopes_10@yahoo.com.br>
2023-02-22 12:42:03 -0300

    feat: create endpoints of application

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,11 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateNotificationBodyDto } from '../dtos/create-notification-body.dto';
 import { SendNotification } from '@application/use-cases/send-notification.use-case';
 import { NotificationViewModel } from '../view-models/notification.view-model';
+import { CancelNotification } from '@application/use-cases/cancel-notification.use-case';
+import { ReadNotification } from '@application/use-cases/read-notification.use-case';
+import { UnreadNotification } from '@application/use-cases/unread-notification.use-case';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications.use-case';
+import { GetRecipientNotifications } from '@application/use-cases/get-notifications.use-case';
 
 @Controller('notifications')
 export class NotificationController {
-  constructor(private readonly sendNotification: SendNotification) {}
+  constructor(
+    private readonly sendNotification: SendNotification,
+    private readonly cancelNotification: CancelNotification,
+    private readonly readNofication: ReadNotification,
+    private readonly unreadNotification: UnreadNotification,
+    private readonly countRecipientNotifications: CountRecipientNotifications,
+    private readonly getRecipientNotifications: GetRecipientNotifications,
+  ) {}
   @Post()
   async create(
     @Body() { recipientId, content, category }: CreateNotificationBodyDto,
@@ -16,5 +37,48 @@ export class NotificationController {
       category,
     });
     return { notification: NotificationViewModel.toHTTP(notification) };
+  }
+
+  @Patch(':id/cancel')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async cancel(@Param('id') id: string): Promise<void> {
+    await this.cancelNotification.execute({ notificationId: id });
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(
+    @Param('recipientId') recipientId: string,
+  ): Promise<{ count: number }> {
+    const count = await this.countRecipientNotifications.execute({
+      recipientId,
+    });
+    return count;
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string): Promise<{
+    notifications: {
+      id: string;
+      content: string;
+      category: string;
+      recipientId: string;
+    }[];
+  }> {
+    const { notifications } = await this.getRecipientNotifications.execute({
+      recipientId,
+    });
+    return { notifications: notifications.map(NotificationViewModel.toHTTP) };
+  }
+
+  @Patch(':id/read')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async read(@Param('id') id: string): Promise<void> {
+    await this.readNofication.execute({ notificationId: id });
+  }
+
+  @Patch(':id/unread')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async unread(@Param('id') id: string): Promise<void> {
+    await this.unreadNotification.execute({ notificationId: id });
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,6 +3,11 @@ import { NotificationRepository } from '@application/repositories/notification.r
 import { SendNotification } from '@application/use-cases/send-notification.use-case';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from './controllers/notification.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification.use-case';
+import { ReadNotification } from '@application/use-cases/read-notification.use-case';
+import { UnreadNotification } from '@application/use-cases/unread-notification.use-case';
+import { GetRecipientNotifications } from '@application/use-cases/get-notifications.use-case';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications.use-case';
 
 @Module({
   imports: [DatabaseModule],
@@ -12,6 +17,41 @@ import { NotificationController } from './controllers/notification.controller';
       provide: SendNotification,
       useFactory: (notificationRepository: NotificationRepository) => {
         return new SendNotification(notificationRepository);
+      },
+      inject: [NotificationRepository],
+    },
+    {
+      provide: CancelNotification,
+      useFactory: (notificationRepository: NotificationRepository) => {
+        return new CancelNotification(notificationRepository);
+      },
+      inject: [NotificationRepository],
+    },
+    {
+      provide: ReadNotification,
+      useFactory: (notificationRepository: NotificationRepository) => {
+        return new ReadNotification(notificationRepository);
+      },
+      inject: [NotificationRepository],
+    },
+    {
+      provide: UnreadNotification,
+      useFactory: (notificationRepository: NotificationRepository) => {
+        return new UnreadNotification(notificationRepository);
+      },
+      inject: [NotificationRepository],
+    },
+    {
+      provide: GetRecipientNotifications,
+      useFactory: (notificationRepository: NotificationRepository) => {
+        return new GetRecipientNotifications(notificationRepository);
+      },
+      inject: [NotificationRepository],
+    },
+    {
+      provide: CountRecipientNotifications,
+      useFactory: (notificationRepository: NotificationRepository) => {
+        return new CountRecipientNotifications(notificationRepository);
       },
       inject: [NotificationRepository],
     },
